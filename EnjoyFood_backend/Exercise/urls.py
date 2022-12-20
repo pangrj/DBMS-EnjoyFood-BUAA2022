50ba758c852038fae40da531f81c24fc83e34ae4
pangy <20373219@buaa.edu.cn>
2022-12-20 16:32:57 +0800

    更新了建立计划

--- EnjoyFood_backend/Exercise/urls.py
+++ EnjoyFood_backend/Exercise/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from Exercise.views import *
 
 urlpatterns = [
-    path('menu/', showExerciseMenu)
+    path('menu/', showExerciseMenu),
+    path('searchByCalorie/', searchByCalorie),
+    path('searchByCircle/', searchByCircle),
+    path('searchByDifficulty/', searchByDifficulty)
 ]
--- EnjoyFood_backend/Plan/urls.py
+++ EnjoyFood_backend/Plan/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from Plan.views import *
 
 urlpatterns = [
-    path('getInfor/', get_plan_details)
+    path('getInfor/', get_plan_details),
+    path('addPlan/', add_plan)
 ]
