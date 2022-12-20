85cec458536689fb301bcd9cce71a32eb935afd4
pangy <20373219@buaa.edu.cn>
2022-12-20 16:25:42 +0800

    更新了展示运动

--- EnjoyFood_backend/Exercise/models.py
+++ EnjoyFood_backend/Exercise/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from ExerArea.models import ExerArea
+
 
 # Create your models here.
 class Exercise(models.Model):
@@ -8,6 +10,7 @@ class Exercise(models.Model):
     sp_difficulty = models.CharField(max_length=20, null=False)
     sp_calories = models.IntegerField(null=False)
     sp_time = models.IntegerField(null=False)
+    exerArea = models.ForeignKey(ExerArea, on_delete=models.CASCADE, null=True)
 
     def get_sp_id(self):
         return self.id
--- EnjoyFood_backend/Exercise/views.py
+++ EnjoyFood_backend/Exercise/views.py
@@ -19,6 +19,64 @@ def showExerciseMenu(request):
         ret.code = 200
         ret.message = "Show Success!"
         ret.load_data({'sports': retExercises})
+        return JsonResponse(ret.json_type())
+    else:
+        ret.Http_error()
+        return JsonResponse(ret.json_type())
+
+
+def searchByCalorie(request):
+    ret = RET.get_instance()
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        u_name = data.get('u_name')
+        sp_calories = data.get('sp_calories')
+
+        sports = Exercise.objects.filter(sp_calories__gte=sp_calories)
+        retSports = serializers.serialize('json', list(sports))
+
+        ret.code = 200
+        ret.message = 'Find Exercise!'
+        ret.load_data({'sports': retSports})
+        return JsonResponse(ret.json_type())
+    else:
+        ret.Http_error()
+        return JsonResponse(ret.json_type())
+
+
+def searchByCircle(request):
+    ret = RET.get_instance()
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        u_name = data.get('u_name')
+        c_name = data.get('c_name')
+
+        sports = Exercise.objects.filter(exerArea__lifeCircle__c_name=c_name)
+        retSports = serializers.serialize('json', list(sports))
+
+        ret.code = 200
+        ret.message = 'Find Exercise!'
+        ret.load_data({'sports': retSports})
+        return JsonResponse(ret.json_type())
+    else:
+        ret.Http_error()
+        return JsonResponse(ret.json_type())
+
+
+def searchByDifficulty(request):
+    ret = RET.get_instance()
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        u_name = data.get('u_name')
+        sp_difficulty = data.get('sp_difficulty')
+
+        sports = Exercise.objects.filter(sp_difficulty=sp_difficulty)
+        retSports = serializers.serialize('json', list(sports))
+
+        ret.code = 200
+        ret.message = 'Find Exercise!'
+        ret.load_data({'sports': retSports})
+        return JsonResponse(ret.json_type())
     else:
         ret.Http_error()
         return JsonResponse(ret.json_type())
