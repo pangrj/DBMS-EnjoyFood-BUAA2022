c13a4bcfaa5ca72d935db4d6e5806f87e3b835f4
pangy <20373219@buaa.edu.cn>
2022-12-21 16:47:34 +0800

    更新了接口以及admin显示

--- EnjoyFood_backend/Dish/admin.py
+++ EnjoyFood_backend/Dish/admin.py
@@ -5,7 +5,7 @@ from Dish.models import *
 
 # Register your models here.
 class DishAdmin(admin.ModelAdmin):
-    list_display = ['d_id', 'd_name', 'd_category', 'd_cuisine', 'd_calories', 'd_price']
+    list_display = ['d_id', 'd_name', 'd_category', 'd_cuisine', 'd_calories', 'd_price', 'restaurant', 'restaurant_id']
 
 
 admin.site.register(Dish, DishAdmin)
